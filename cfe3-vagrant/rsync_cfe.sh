7f06ad6524d219a0ffdd8d9f17e7bbdfbb8df381
E Camden Fisher <fish@fishnix.net>
2013-03-18 15:55:17 -0400

    rsync cfe policy

---
+++ cfe3-vagrant/rsync_cfe.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Hackety Hack rsync to bring in modified policy
+#
+
+SOURCE='/Volumes/cfengine3/'
+
+rsync --delete -av --exclude='*promises.cf*' --exclude='*group.cf*' --exclude='.Trashes' ${SOURCE}  cfengine3/
